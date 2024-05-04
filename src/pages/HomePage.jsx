8860680fe9d9a01d1e6abbdcb93fdbcc722a40f8
supriya224 <52038704+supriya224@users.noreply.github.com>
2024-05-05 00:16:16 +0530

    init

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import classNames from "classnames";
+import styles from "./page.module.css";
+
+export const HomePage = () => {
+  return (
+    <div className={classNames(styles.about_page)}>HomePage</div>
+  )
+}
